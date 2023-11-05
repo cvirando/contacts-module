5e5d67cc72f3aa789b32b6d0298fefc1ead4f222
robertnicjoo <robertnicjoo@outlook.com>
2023-11-05 13:52:16 +0700

    fixed routing issue

--- Resources/views/index.blade.php
+++ Resources/views/index.blade.php
@@ -25,11 +25,6 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <p>
-                    <a class="btn btn-green" href="{{route('payrollSettings')}}">Settings</a>
-                </p>
-            </div>
-            <div class="col-md-12">
                 <div class="card">
                     <div class="card-body">
                         <h5 class="card-title">
